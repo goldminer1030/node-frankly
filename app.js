0fdd31b17b729570cb9d4b4b5f1609f871cfed86
goldminer1030 <goldminer1030@gmail.com>
2018-03-09 11:55:01 +0100

    test

--- app.js
+++ app.js
@@ -82,9 +82,10 @@ app.use(function (req, res, next) {
   if (req.session) {
     console.log('req.session');
     if(req.session.passport && req.session.passport.user) {
-      console.log('req.session.passport', req.session.passport.user.username);
+      var user = req.session.passport.user;
+      console.log('req.session.passport.user', user);
       res.locals.login = true;
-      res.locals.username = req.session.passport.user.username;
+      res.locals.username = user.username;
       res.locals.showProfile = true;
       console.log('res.locals.username ---', res.locals.username);
     }
