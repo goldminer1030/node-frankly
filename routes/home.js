181957adc47a9c78d56eb5ead9326ba03da9c244
goldminer1030 <goldminer1030@gmail.com>
2018-03-09 07:40:34 +0100

    debug step

--- routes/home.js
+++ routes/home.js
@@ -20,7 +20,7 @@ router.get('/', function (req, res) {
     console.log('subdomain', subdomain);
     User.findOne({ username: subdomain }, function (err, user) {
       if (!err && user) {
-        if ((req.user && subdomain == req.user.username) || (res.locals.username.lenth > 0 && res.locals.username == subdomain)) {
+        if ((req.user && subdomain == req.user.username) || res.locals.username == subdomain) {
           Message.find({ 'username': subdomain }).sort({ 'createdAt': -1 }).exec(function (err, message) {
             if (!err && message) {
               res.render('users/message', {
